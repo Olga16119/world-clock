3c2f6bc9b5804879bf4618bdb8e30a5bbcc22589
Olga16119 <tkachenko93@ukr.net>
2024-09-15 21:05:49 +0200

    added function updateTime

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+
+  losAngelesDate.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTime.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let parisElement = document.querySelector("#paris");
+  let parisDate = parisElement.querySelector(".date");
+  let parisTime = parisElement.querySelector(".time");
+
+  parisDate.innerHTML = moment().format("MMMM Do YYYY");
+  parisTime.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
